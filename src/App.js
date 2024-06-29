f9f4c64b11de53c7db12a56bbc7588928c1cd3c8
ahyng <ahyng1@gmail.com>
2024-06-30 00:03:13 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./style.css";
 
 function App() {
+  const [number, setNumber] = useState(Math.floor(Math.random() * 89 + 10));
+  const [inputNum, setInputNum] = useState('');
+  const [upDown, setUpDown] = useState("1부터 100까지의 수를 맞춰보세요!");
+  const [reset, setReset] = useState(false);
+
+  const getInput = (event) => {
+    setInputNum(event.target.value);
+  }
+
+  const resetGame = () => {
+    setNumber(Math.floor(Math.random() * 99 + 1));
+    setReset(false);
+    setUpDown("1부터 100까지의 수를 맞춰보세요!");
+  }
+
+  const guessNum = (event) => {
+    event.preventDefault();
+    console.log(inputNum);
+
+    if (inputNum == "" || inputNum <= 0 || inputNum > 100){
+      alert("1부터 100까지의 수를 입력해주세요!");
+      
+    } else {
+      if (inputNum == number){
+        setUpDown(`${inputNum} 정답!`);
+        setReset(true);
+      }
+      else if (inputNum > number) {
+        setUpDown("Down");
+      } else {
+        setUpDown("Up");
+      }
+    }
+
+    setInputNum("");
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="total">
+      <h4 className="title">Up and Down</h4>
+      <div className="App">
+        {reset ? 
+          <div>
+            <h3>{upDown}</h3> 
+            <button onClick={resetGame}>reset</button>
+          </div>
+
+          : 
+          
+          <form onSubmit={guessNum}>
+            <input className="inputBox" value={inputNum} type="number" onChange={getInput}/>
+            <button>enter</button>
+            <h4>{upDown}</h4>
+          </form>
+        }
+        
+      </div>
     </div>
   );
 }
