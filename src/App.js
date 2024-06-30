80ff26aacbeba7d7f8a006834ff9c901b7d5d10f
ahyng <ahyng1@gmail.com>
2024-07-01 00:19:40 +0900

    input값 숫자로 가져오기 + backspace시 0이 되는 오류 해결

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import "./style.css";
 
 function App() {
   const [number, setNumber] = useState(Math.floor(Math.random() * 89 + 10));
-  const [inputNum, setInputNum] = useState('');
+  const [inputNum, setInputNum] = useState("");
   const [upDown, setUpDown] = useState("1부터 100까지의 수를 맞춰보세요!");
   const [reset, setReset] = useState(false);
   const [count, setCount] = useState(0);
   const [gameOverCheck, setGameOver] = useState(false);
 
   const getInput = (event) => {
-    setInputNum(Number(event.target.value));
+    if (isNaN(event.target.value)) {
+      alert('1부터 100까지의 수를 입력해주세요!');
+    } else if (event.target.value == ''){
+      setInputNum('');
+    } else {
+      setInputNum(Number(event.target.value));
+    }
   }
 
   const resetGame = () => {
@@ -29,11 +35,12 @@ function App() {
 
   const guessNum = (event) => {
     event.preventDefault();
-
-    if (inputNum === "" || inputNum <= 0 || inputNum > 100){
+    
+    if (!(inputNum >= 1 && inputNum <= 100)){
       alert("1부터 100까지의 수를 입력해주세요!");
     } 
     else {
+
       setCount((current) => (current + 1));
 
       if (count === 9) {
@@ -70,13 +77,13 @@ function App() {
           : 
           
           <form onSubmit={guessNum}>
-            <input className="inputBox" value={inputNum} type="number" onChange={getInput}/>
+            <input className="inputBox" value={inputNum} onChange={getInput}/>
             <button>enter</button>
             <h4 className="guide">{upDown}</h4>
           </form>
         }
 
-        { gameOverCheck ? null : <h6 className="gameCount">남은 기회 : {10 - count}</h6>}
+        { gameOverCheck ? null : <h6 className="gameCount">남은 기회 : {count === 9 ? <span style={{color : 'red'}}>{10 - count}</span> : <span>{10 - count}</span>}</h6>}
         
       </div>
     </div>
